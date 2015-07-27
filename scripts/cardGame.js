633db650c9449fe31fdfd047f4d5d2ad71db109b
vvn050 <nikolov.vladislav@gmail.com>
2015-07-27 14:18:28 +0300

    added CustomAlert

--- scripts/cardGame.js
+++ scripts/cardGame.js
@@ -415,13 +415,85 @@ var Game = (function () {
     function checkMagicValue(magicValue) {
         if (magicValue < MAGIC_VALUE.MIN || magicValue > MAGIC_VALUE.MAX
             || isNaN(magicValue) || magicValue == null) {
-            window.alert('Magic value should be a number between 1 and 27');
+            alert.render('Magic value should be a number between 1 and 27');
             return false;
         }
         else {
             return true;
         }
     }
+
+
+    var dialogBox = document.createElement('div'),
+        dialogOverLay = document.createElement('div'),
+        nestedDialogBox = document.createElement('div'),
+        dialogAlertHeader = document.createElement('div'),
+        dialogAlertBody = document.createElement('div'),
+        dialogAlertFooter = document.createElement('div');
+
+    dialogBox.appendChild(nestedDialogBox);
+    nestedDialogBox.appendChild(dialogAlertHeader);
+    nestedDialogBox.appendChild(dialogAlertBody);
+    nestedDialogBox.appendChild(dialogAlertFooter);
+
+    dialogOverLay.style.display = 'none';
+    dialogOverLay.style.opacity = '0.8';
+    dialogOverLay.style.position = 'fixed';
+    dialogOverLay.style.top = '0px';
+    dialogOverLay.style.left = '0px';
+    dialogOverLay.style.background = '#FFF';
+    dialogOverLay.style.width = '100%';
+    dialogOverLay.style.zIndex = '10';
+
+    dialogBox.style.display = 'none';
+    dialogBox.style.position = 'fixed';
+    dialogBox.style.background = '#000';
+    dialogBox.style.borderRadius = '9px';
+    dialogBox.style.width = '550px';
+    dialogBox.style.zIndex = '10';
+
+    dialogAlertHeader.style.background = '#666';
+    dialogAlertHeader.style.fontSize = '19px';
+    dialogAlertHeader.style.padding = '10px';
+    dialogAlertHeader.style.color = '#CCC';
+
+    dialogAlertBody.style.background = '#333';
+    dialogAlertBody.style.padding = '20px';
+    dialogAlertBody.style.color = '#FFF';
+
+    dialogAlertFooter.style.background = '#666';
+    dialogAlertFooter.style.padding = '10px';
+    dialogAlertFooter.style.textAlign = 'right';
+
+
+    document.body.appendChild(dialogOverLay);
+    document.body.appendChild(dialogBox);
+
+    var alert = new CustomAlert();
+
+
+    function CustomAlert() {
+        this.render = function (dialog) {
+            var winW = window.innerWidth;
+            var winH = window.innerHeight;
+
+            dialogOverLay.style.display = "block";
+            dialogOverLay.style.height = winH + "px";
+            dialogBox.style.left = (winW / 2) - (550 * .5) + "px";
+            dialogBox.style.top = "100px";
+            dialogBox.style.display = "block";
+            dialogAlertHeader.innerHTML = "Message from the magician:";
+            dialogAlertBody.innerHTML = dialog;
+            dialogAlertFooter.innerHTML = '<button id="btnOK">OK</button>';
+            var btnOK = document.getElementById('btnOK');
+            btnOK.addEventListener('click', alert.ok);
+        },
+            this.ok = function () {
+                dialogBox.style.display = "none";
+                dialogOverLay.style.display = "none";
+            }
+    }
+
 
     function drawText(currentCard, context) {
         context.clearRect(0, 0, context.canvas.width, context.canvas.height);
