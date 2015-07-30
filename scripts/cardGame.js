3e1a34317614bdc2a6e466ffa5f35cd3f60be8c0
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-07-30 16:13:18 +0300

    extracted ThreePots in separate js file

--- scripts/cardGame.js
+++ scripts/cardGame.js
@@ -4,226 +4,7 @@ var Game = (function () {
 
     var Deck = deckObj();
 
-    var ThreePots = (function () {
-        var ThreePots = {
-            init: function () {
-
-                return this;
-            },
-            getThreePots: function (deck) {
-                var currentDeck = deck.slice();
-                var firstPot = [],
-                    secondPot = [],
-                    thirdPot = [];
-                var currentCard = {};
-
-                for (var i = 0; i < NUMBER_OF_CARDS.POT; i++) {
-                    currentCard = Deck.getRandomCard(currentDeck);
-                    firstPot.push(currentCard);
-                    currentDeck = Deck.deleteDrawedCard(currentDeck, currentCard);
-
-                    currentCard = Deck.getRandomCard(currentDeck);
-                    secondPot.push(currentCard);
-                    currentDeck = Deck.deleteDrawedCard(currentDeck, currentCard);
-
-                    currentCard = Deck.getRandomCard(currentDeck);
-                    thirdPot.push(currentCard);
-                    currentDeck = Deck.deleteDrawedCard(currentDeck, currentCard);
-                }
-                return {
-                    firstPot: firstPot.slice(),
-                    secondPot: secondPot.slice(),
-                    thirdPot: thirdPot.slice()
-                };
-            },
-            potDealer: function (number) {
-                var logicForNumbers = ['000', '100', '200',
-                    '010', '110', '210',
-                    '020', '120', '220',
-                    '001', '101', '201',
-                    '011', '111', '211',
-                    '021', '121', '221',
-                    '002', '102', '202',
-                    '012', '112', '212',
-                    '022', '122', '222'];
-                var logic = logicForNumbers[number - 1];
-                var potTurns = '';
-                for (var i = 0; i < logic.length; i++) {
-                    if (logic[i] === '0') {
-                        potTurns += 'TOP';
-                        potTurns += " ";
-                    } else if (logic[i] === '1') {
-                        potTurns += 'MIDDLE';
-                        potTurns += " ";
-                    } else if (logic[i] === '2') {
-                        potTurns += 'DOWN';
-                        potTurns += " ";
-                    }
-
-
-                }
-                return potTurns;
-            },
-            dealThreePots: function (threePots, context) {
-
-
-                var firstPot = threePots.firstPot,
-                    secondPot = threePots.secondPot,
-                    thirdPot = threePots.thirdPot,
-                    i;
-
-                var giveThreePots = setInterval(function () {
-                    threePotsTimer()
-                }, TIMERS.GIVE_CARDS_MS);
-                i = 0;
-
-                function threePotsTimer() {
-                    Card.drawCard(firstPot[i], context, CARD_POS.POTS_START_X + i * CARD_POS.POTS_SPACING, CARD_POS.POT_1_Y, CARD_DIM.WIDTH, CARD_DIM.HEIGHT);
-                    Card.drawCard(secondPot[i], context, CARD_POS.POTS_START_X + i * CARD_POS.POTS_SPACING, CARD_POS.POT_2_Y, CARD_DIM.WIDTH, CARD_DIM.HEIGHT);
-                    Card.drawCard(thirdPot[i], context, CARD_POS.POTS_START_X + i * CARD_POS.POTS_SPACING, CARD_POS.POT_3_Y, CARD_DIM.WIDTH, CARD_DIM.HEIGHT);
-                    i++;
-                    if (i === NUMBER_OF_CARDS.POT) {
-                        clearInterval(giveThreePots);
-                    }
-                }
-
-                return {
-                    firstPot: firstPot.slice(),
-                    secondPot: secondPot.slice(),
-                    thirdPot: thirdPot.slice()
-                };
-            },
-            shuffleCards: function () {
-                var i;
-                currentMixedPots = {
-                    firstPot: [],
-                    secondPot: [],
-                    thirdPot: []
-                };
-                for (i = 0; i < NUMBER_OF_CARDS.POT; i++) {
-                    if (i % 3 === 0) {
-                        currentMixedPots.firstPot.push(currentThreePots.firstPot[i]);
-                    }
-                    else if (i % 3 === 1) {
-                        currentMixedPots.secondPot.push(currentThreePots.firstPot[i]);
-                    }
-                    else if (i % 3 === 2) {
-                        currentMixedPots.thirdPot.push(currentThreePots.firstPot[i]);
-                    }
-                }
-                for (i = 0; i < NUMBER_OF_CARDS.POT; i++) {
-                    if (i % 3 === 0) {
-                        currentMixedPots.firstPot.push(currentThreePots.secondPot[i]);
-                    }
-                    else if (i % 3 === 1) {
-                        currentMixedPots.secondPot.push(currentThreePots.secondPot[i]);
-                    }
-                    else if (i % 3 === 2) {
-                        currentMixedPots.thirdPot.push(currentThreePots.secondPot[i]);
-                    }
-                }
-                for (i = 0; i < NUMBER_OF_CARDS.POT; i++) {
-                    if (i % 3 === 0) {
-                        currentMixedPots.firstPot.push(currentThreePots.thirdPot[i]);
-                    }
-                    else if (i % 3 === 1) {
-                        currentMixedPots.secondPot.push(currentThreePots.thirdPot[i]);
-                    }
-                    else if (i % 3 === 2) {
-                        currentMixedPots.thirdPot.push(currentThreePots.thirdPot[i]);
-                    }
-                }
-                currentThreePots.firstPot = currentMixedPots.firstPot.slice();
-                currentThreePots.secondPot = currentMixedPots.secondPot.slice();
-                currentThreePots.thirdPot = currentMixedPots.thirdPot.slice();
-                threePots.firstPot = currentMixedPots.firstPot.slice();
-                threePots.secondPot = currentMixedPots.secondPot.slice();
-                threePots.thirdPot = currentMixedPots.thirdPot.slice();
-            },
-            PutFirstOnPlace: function (turn) {
-
-                if (currentPotTurns[turn] === 'TOP') {//put the current pot on the top
-                    console.log('top');
-                    currentThreePots.firstPot = threePots.firstPot.slice();
-                    currentThreePots.secondPot = threePots.secondPot.slice();
-                    currentThreePots.thirdPot = threePots.thirdPot.slice();
-                }
-                if (currentPotTurns[turn] === 'MIDDLE') {//put the current pot in the middle
-                    console.log('middle');
-                    currentThreePots.secondPot = threePots.firstPot.slice();
-                    currentThreePots.firstPot = threePots.secondPot.slice();
-                    currentThreePots.thirdPot = threePots.thirdPot.slice();
-                }
-                if (currentPotTurns[turn] === 'DOWN') {//put the current pot on the bottom
-                    console.log('bottom');
-                    currentThreePots.thirdPot = threePots.firstPot.slice();
-                    currentThreePots.firstPot = threePots.thirdPot.slice();
-                    currentThreePots.secondPot = threePots.secondPot.slice();
-                }
-                threePots.firstPot = currentThreePots.firstPot.slice();
-                threePots.secondPot = currentThreePots.secondPot.slice();
-                threePots.thirdPot = currentThreePots.thirdPot.slice();
-
-                gameInstructions(gameInstructionsCounter);
-                gameInstructionsCounter++;
-            },
-            PutSecondOnPlace: function (turn) {
-
-                if (currentPotTurns[turn] === 'TOP') {//put the current pot on the top
-                    console.log('top');
-                    currentThreePots.firstPot = threePots.secondPot.slice();
-                    currentThreePots.secondPot = threePots.firstPot.slice();
-                    currentThreePots.thirdPot = threePots.thirdPot.slice();
-                }
-                if (currentPotTurns[turn] === 'MIDDLE') {//put the current pot in the middle
-                    console.log('middle');
-                    currentThreePots.secondPot = threePots.secondPot.slice();
-                    currentThreePots.firstPot = threePots.firstPot.slice();
-                    currentThreePots.thirdPot = threePots.thirdPot.slice();
-                }
-                if (currentPotTurns[turn] === 'DOWN') {//put the current pot on the bottom
-                    console.log('bottom');
-                    currentThreePots.thirdPot = threePots.secondPot.slice();
-                    currentThreePots.firstPot = threePots.firstPot.slice();
-                    currentThreePots.secondPot = threePots.thirdPot.slice();
-                }
-                threePots.firstPot = currentThreePots.firstPot.slice();
-                threePots.secondPot = currentThreePots.secondPot.slice();
-                threePots.thirdPot = currentThreePots.thirdPot.slice();
-
-                gameInstructions(gameInstructionsCounter);
-                gameInstructionsCounter++;
-            },
-            PutThirdOnPlace: function (turn) {
-
-                if (currentPotTurns[turn] === 'TOP') {//put the current pot on the top
-                    console.log('top');
-                    currentThreePots.firstPot = threePots.thirdPot.slice();
-                    currentThreePots.secondPot = threePots.secondPot.slice();
-                    currentThreePots.thirdPot = threePots.firstPot.slice();
-                }
-                if (currentPotTurns[turn] === 'MIDDLE') {//put the current pot in the middle
-                    console.log('middle');
-                    currentThreePots.secondPot = threePots.thirdPot.slice();
-                    currentThreePots.firstPot = threePots.firstPot.slice();
-                    currentThreePots.thirdPot = threePots.secondPot.slice();
-                }
-                if (currentPotTurns[turn] === 'DOWN') {//put the current pot on the bottom
-                    console.log('bottom');
-                    currentThreePots.thirdPot = threePots.thirdPot.slice();
-                    currentThreePots.firstPot = threePots.firstPot.slice();
-                    currentThreePots.secondPot = threePots.secondPot.slice();
-                }
-                threePots.firstPot = currentThreePots.firstPot.slice();
-                threePots.secondPot = currentThreePots.secondPot.slice();
-                threePots.thirdPot = currentThreePots.thirdPot.slice();
-
-                gameInstructions(gameInstructionsCounter);
-                gameInstructionsCounter++;
-            }
-        };
-        return ThreePots;
-    }());
+    var ThreePots = threePotsObj();
 
     Deck.init('Manhattan');
 
@@ -234,7 +15,7 @@ var Game = (function () {
         areShuffled,
         buttonClickCount = 0,
         cardDeck = Deck.fillDeckWithCards(Card),
-        threePots = ThreePots.getThreePots(cardDeck),
+        threePots = ThreePots.getThreePots(cardDeck, Deck),
         currentPotTurns,
         gameInstructionsCounter = 0,
         currentMixedPots = {
@@ -432,7 +213,7 @@ var Game = (function () {
             $('#submit-btn').css('display', 'none');
             $('#btnDrawPots').css('display', 'none');
             context.clearRect(0, 0, cardCanvas.width, cardCanvas.height);
-            ThreePots.dealThreePots(threePots, context);
+            ThreePots.dealThreePots(threePots, context, Card);
             $('#canvasContainer').append('<input type="button" class="invisible-button" id="btnChoosePot1" value="1">');
             $('#canvasContainer').append('<input type="button" class="invisible-button" id="btnChoosePot2" value="2">');
             $('#canvasContainer').append('<input type="button" class="invisible-button" id="btnChoosePot3" value="3">');
@@ -447,10 +228,10 @@ var Game = (function () {
 
             buttonChoosePot1.addEventListener('click', function () {
 
-                ThreePots.PutFirstOnPlace(buttonClickCount);
+                ThreePots.PutFirstOnPlace(buttonClickCount, currentPotTurns, currentThreePots, threePots);
 
                 if (buttonClickCount < 2) {
-                    ThreePots.shuffleCards();
+                    ThreePots.shuffleCards(currentThreePots, threePots);
                 }
 
                 buttonClickCount++;
@@ -460,16 +241,16 @@ var Game = (function () {
                 }
                 else {
                     context.clearRect(0, 0, cardCanvas.width, cardCanvas.height);
-                    ThreePots.dealThreePots(currentThreePots, context);
+                    ThreePots.dealThreePots(currentThreePots, context, Card);
                 }
             }, false);
 
             buttonChoosePot2.addEventListener('click', function () {
 
-                ThreePots.PutSecondOnPlace(buttonClickCount);
+                ThreePots.PutSecondOnPlace(buttonClickCount, currentPotTurns, currentThreePots, threePots);
 
                 if (buttonClickCount < 2) {
-                    ThreePots.shuffleCards();
+                    ThreePots.shuffleCards(currentThreePots, threePots);
                 }
 
                 buttonClickCount++;
@@ -479,16 +260,16 @@ var Game = (function () {
                 }
                 else {
                     context.clearRect(0, 0, cardCanvas.width, cardCanvas.height);
-                    ThreePots.dealThreePots(currentThreePots, context);
+                    ThreePots.dealThreePots(currentThreePots, context, Card);
                 }
             }, false);
 
             buttonChoosePot3.addEventListener('click', function () {
 
-                ThreePots.PutThirdOnPlace(buttonClickCount);
+                ThreePots.PutThirdOnPlace(buttonClickCount, currentPotTurns, currentThreePots, threePots);
 
                 if (buttonClickCount < 2) {
-                    ThreePots.shuffleCards();
+                    ThreePots.shuffleCards(currentThreePots, threePots);
                 }
 
                 buttonClickCount++;
@@ -498,7 +279,7 @@ var Game = (function () {
                 }
                 else {
                     context.clearRect(0, 0, cardCanvas.width, cardCanvas.height);
-                    ThreePots.dealThreePots(currentThreePots, context);
+                    ThreePots.dealThreePots(currentThreePots, context, Card);
                 }
             }, false);
 
---
+++ scripts/threePots.js
@@ -0,0 +1,212 @@
+function threePotsObj() {
+    var ThreePots = {
+        init: function() {
+
+            return this;
+        },
+        getThreePots: function(cardDeck, Deck) {
+            var currentDeck = cardDeck.slice(),
+                firstPot = [],
+                secondPot = [],
+                thirdPot = [],
+                currentCard = {};
+
+            for (var i = 0; i < NUMBER_OF_CARDS.POT; i++) {
+                currentCard = Deck.getRandomCard(currentDeck);
+                firstPot.push(currentCard);
+                currentDeck = Deck.deleteDrawedCard(currentDeck, currentCard);
+
+                currentCard = Deck.getRandomCard(currentDeck);
+                secondPot.push(currentCard);
+                currentDeck = Deck.deleteDrawedCard(currentDeck, currentCard);
+
+                currentCard = Deck.getRandomCard(currentDeck);
+                thirdPot.push(currentCard);
+                currentDeck = Deck.deleteDrawedCard(currentDeck, currentCard);
+            }
+            return {
+                firstPot: firstPot.slice(),
+                secondPot: secondPot.slice(),
+                thirdPot: thirdPot.slice()
+            };
+        },
+        potDealer: function(number) {
+            var logicForNumbers = ['000', '100', '200',
+                '010', '110', '210',
+                '020', '120', '220',
+                '001', '101', '201',
+                '011', '111', '211',
+                '021', '121', '221',
+                '002', '102', '202',
+                '012', '112', '212',
+                '022', '122', '222'
+            ];
+            var logic = logicForNumbers[number - 1];
+            var potTurns = '';
+            for (var i = 0; i < logic.length; i++) {
+                if (logic[i] === '0') {
+                    potTurns += 'TOP';
+                    potTurns += " ";
+                } else if (logic[i] === '1') {
+                    potTurns += 'MIDDLE';
+                    potTurns += " ";
+                } else if (logic[i] === '2') {
+                    potTurns += 'DOWN';
+                    potTurns += " ";
+                }
+
+
+            }
+            return potTurns;
+        },
+        dealThreePots: function(threePots, context, Card) {
+            var firstPot = threePots.firstPot,
+                secondPot = threePots.secondPot,
+                thirdPot = threePots.thirdPot,
+                i = 0;
+
+            var giveThreePots = setInterval(function() {
+                threePotsTimer(Card);
+            }, TIMERS.GIVE_CARDS_MS);
+
+            function threePotsTimer(Card) {
+                Card.drawCard(firstPot[i], context, CARD_POS.POTS_START_X + i * CARD_POS.POTS_SPACING, CARD_POS.POT_1_Y, CARD_DIM.WIDTH, CARD_DIM.HEIGHT);
+                Card.drawCard(secondPot[i], context, CARD_POS.POTS_START_X + i * CARD_POS.POTS_SPACING, CARD_POS.POT_2_Y, CARD_DIM.WIDTH, CARD_DIM.HEIGHT);
+                Card.drawCard(thirdPot[i], context, CARD_POS.POTS_START_X + i * CARD_POS.POTS_SPACING, CARD_POS.POT_3_Y, CARD_DIM.WIDTH, CARD_DIM.HEIGHT);
+                i++;
+                if (i === NUMBER_OF_CARDS.POT) {
+                    clearInterval(giveThreePots);
+                }
+            }
+
+            return {
+                firstPot: firstPot.slice(),
+                secondPot: secondPot.slice(),
+                thirdPot: thirdPot.slice()
+            };
+        },
+        shuffleCards: function(currentThreePots, threePots) {
+            var i;
+            currentMixedPots = {
+                firstPot: [],
+                secondPot: [],
+                thirdPot: [],
+            };
+            for (i = 0; i < NUMBER_OF_CARDS.POT; i++) {
+                if (i % 3 === 0) {
+                    currentMixedPots.firstPot.push(currentThreePots.firstPot[i]);
+                } else if (i % 3 === 1) {
+                    currentMixedPots.secondPot.push(currentThreePots.firstPot[i]);
+                } else if (i % 3 === 2) {
+                    currentMixedPots.thirdPot.push(currentThreePots.firstPot[i]);
+                }
+            }
+            for (i = 0; i < NUMBER_OF_CARDS.POT; i++) {
+                if (i % 3 === 0) {
+                    currentMixedPots.firstPot.push(currentThreePots.secondPot[i]);
+                } else if (i % 3 === 1) {
+                    currentMixedPots.secondPot.push(currentThreePots.secondPot[i]);
+                } else if (i % 3 === 2) {
+                    currentMixedPots.thirdPot.push(currentThreePots.secondPot[i]);
+                }
+            }
+            for (i = 0; i < NUMBER_OF_CARDS.POT; i++) {
+                if (i % 3 === 0) {
+                    currentMixedPots.firstPot.push(currentThreePots.thirdPot[i]);
+                } else if (i % 3 === 1) {
+                    currentMixedPots.secondPot.push(currentThreePots.thirdPot[i]);
+                } else if (i % 3 === 2) {
+                    currentMixedPots.thirdPot.push(currentThreePots.thirdPot[i]);
+                }
+            }
+            currentThreePots.firstPot = currentMixedPots.firstPot.slice();
+            currentThreePots.secondPot = currentMixedPots.secondPot.slice();
+            currentThreePots.thirdPot = currentMixedPots.thirdPot.slice();
+            threePots.firstPot = currentMixedPots.firstPot.slice();
+            threePots.secondPot = currentMixedPots.secondPot.slice();
+            threePots.thirdPot = currentMixedPots.thirdPot.slice();
+        },
+        PutFirstOnPlace: function(turn, currentPotTurns, currentThreePots, threePots, gameInstructionsCounter) {
+
+            if (currentPotTurns[turn] === 'TOP') { //put the current pot on the top
+                console.log('top');
+                currentThreePots.firstPot = threePots.firstPot.slice();
+                currentThreePots.secondPot = threePots.secondPot.slice();
+                currentThreePots.thirdPot = threePots.thirdPot.slice();
+            }
+            if (currentPotTurns[turn] === 'MIDDLE') { //put the current pot in the middle
+                console.log('middle');
+                currentThreePots.secondPot = threePots.firstPot.slice();
+                currentThreePots.firstPot = threePots.secondPot.slice();
+                currentThreePots.thirdPot = threePots.thirdPot.slice();
+            }
+            if (currentPotTurns[turn] === 'DOWN') { //put the current pot on the bottom
+                console.log('bottom');
+                currentThreePots.thirdPot = threePots.firstPot.slice();
+                currentThreePots.firstPot = threePots.thirdPot.slice();
+                currentThreePots.secondPot = threePots.secondPot.slice();
+            }
+            threePots.firstPot = currentThreePots.firstPot.slice();
+            threePots.secondPot = currentThreePots.secondPot.slice();
+            threePots.thirdPot = currentThreePots.thirdPot.slice();
+
+            gameInstructions(gameInstructionsCounter);
+            gameInstructionsCounter++;
+        },
+        PutSecondOnPlace: function(turn, currentPotTurns, currentThreePots, threePots, gameInstructionsCounter) {
+
+            if (currentPotTurns[turn] === 'TOP') { //put the current pot on the top
+                console.log('top');
+                currentThreePots.firstPot = threePots.secondPot.slice();
+                currentThreePots.secondPot = threePots.firstPot.slice();
+                currentThreePots.thirdPot = threePots.thirdPot.slice();
+            }
+            if (currentPotTurns[turn] === 'MIDDLE') { //put the current pot in the middle
+                console.log('middle');
+                currentThreePots.secondPot = threePots.secondPot.slice();
+                currentThreePots.firstPot = threePots.firstPot.slice();
+                currentThreePots.thirdPot = threePots.thirdPot.slice();
+            }
+            if (currentPotTurns[turn] === 'DOWN') { //put the current pot on the bottom
+                console.log('bottom');
+                currentThreePots.thirdPot = threePots.secondPot.slice();
+                currentThreePots.firstPot = threePots.firstPot.slice();
+                currentThreePots.secondPot = threePots.thirdPot.slice();
+            }
+            threePots.firstPot = currentThreePots.firstPot.slice();
+            threePots.secondPot = currentThreePots.secondPot.slice();
+            threePots.thirdPot = currentThreePots.thirdPot.slice();
+
+            gameInstructions(gameInstructionsCounter);
+            gameInstructionsCounter++;
+        },
+        PutThirdOnPlace: function(turn, currentPotTurns, currentThreePots, threePots, gameInstructionsCounter) {
+
+            if (currentPotTurns[turn] === 'TOP') { //put the current pot on the top
+                console.log('top');
+                currentThreePots.firstPot = threePots.thirdPot.slice();
+                currentThreePots.secondPot = threePots.secondPot.slice();
+                currentThreePots.thirdPot = threePots.firstPot.slice();
+            }
+            if (currentPotTurns[turn] === 'MIDDLE') { //put the current pot in the middle
+                console.log('middle');
+                currentThreePots.secondPot = threePots.thirdPot.slice();
+                currentThreePots.firstPot = threePots.firstPot.slice();
+                currentThreePots.thirdPot = threePots.secondPot.slice();
+            }
+            if (currentPotTurns[turn] === 'DOWN') { //put the current pot on the bottom
+                console.log('bottom');
+                currentThreePots.thirdPot = threePots.thirdPot.slice();
+                currentThreePots.firstPot = threePots.firstPot.slice();
+                currentThreePots.secondPot = threePots.secondPot.slice();
+            }
+            threePots.firstPot = currentThreePots.firstPot.slice();
+            threePots.secondPot = currentThreePots.secondPot.slice();
+            threePots.thirdPot = currentThreePots.thirdPot.slice();
+
+            gameInstructions(gameInstructionsCounter);
+            gameInstructionsCounter++;
+        }
+    };
+    return ThreePots;
+}
