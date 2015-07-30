8b31b0a5364565fef713c6399d0d64ae4047fd58
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-07-30 10:54:36 +0300

    Small changes

--- scripts/cardGame.js
+++ scripts/cardGame.js
@@ -141,33 +141,33 @@ var Game = (function () {
             },
             fillDeckWithCards: function () {
                 var deckOfCard = [];
-                deckOfCard.push(Object.create(Card).init('Ace', suitType.Diamond, 14, 'images/classic-cards/01_of_diamonds_A.svg', 'sounds/cardPlace1.wav'));
-                deckOfCard.push(Object.create(Card).init('Ace', suitType.Clubs, 14, 'images/classic-cards/01_of_clubs_A.svg', 'sounds/cardPlace2.wav'));
-                deckOfCard.push(Object.create(Card).init('Ace', suitType.Heart, 14, 'images/classic-cards/01_of_hearts_A.svg', 'sounds/cardPlace3.wav'));
-                deckOfCard.push(Object.create(Card).init('Ace', suitType.Spade, 14, 'images/classic-cards/01_of_spades_A.svg', 'sounds/cardPlace4.wav'));
-                deckOfCard.push(Object.create(Card).init('King', suitType.Diamond, 13, 'images/classic-cards/King_of_diamonds2.svg', 'sounds/cardPlace1.wav'));
-                deckOfCard.push(Object.create(Card).init('King', suitType.Clubs, 13, 'images/classic-cards/King_of_clubs2.svg', 'sounds/cardPlace2.wav'));
-                deckOfCard.push(Object.create(Card).init('King', suitType.Heart, 13, 'images/classic-cards/King_of_hearts2.svg', 'sounds/cardPlace3.wav'));
-                deckOfCard.push(Object.create(Card).init('King', suitType.Spade, 13, 'images/classic-cards/King_of_spades2.svg', 'sounds/cardPlace4.wav'));
-                deckOfCard.push(Object.create(Card).init('Queen', suitType.Diamond, 12, 'images/classic-cards/Queen_of_diamonds2.svg', 'sounds/cardPlace1.wav'));
-                deckOfCard.push(Object.create(Card).init('Queen', suitType.Clubs, 12, 'images/classic-cards/Queen_of_clubs2.svg', 'sounds/cardPlace2.wav'));
-                deckOfCard.push(Object.create(Card).init('Queen', suitType.Heart, 12, 'images/classic-cards/Queen_of_hearts2.svg', 'sounds/cardPlace3.wav'));
-                deckOfCard.push(Object.create(Card).init('Queen', suitType.Spade, 12, 'images/classic-cards/Queen_of_spades2.svg', 'sounds/cardPlace4.wav'));
-                deckOfCard.push(Object.create(Card).init('Jack', suitType.Diamond, 11, 'images/classic-cards/Jack_of_diamonds2.svg', 'sounds/cardPlace1.wav'));
-                deckOfCard.push(Object.create(Card).init('Jack', suitType.Clubs, 11, 'images/classic-cards/Jack_of_clubs2.svg', 'sounds/cardPlace1.wav'));
-                deckOfCard.push(Object.create(Card).init('Jack', suitType.Heart, 11, 'images/classic-cards/Jack_of_hearts2.svg', 'sounds/cardPlace1.wav'));
-                deckOfCard.push(Object.create(Card).init('Jack', suitType.Spade, 11, 'images/classic-cards/Jack_of_spades2.svg', 'sounds/cardPlace1.wav'));
-                deckOfCard.push(Object.create(Card).init('Ten', suitType.Diamond, 10, 'images/classic-cards/10_of_diamonds_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
-                deckOfCard.push(Object.create(Card).init('Ten', suitType.Clubs, 10, 'images/classic-cards/10_of_clubs_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
-                deckOfCard.push(Object.create(Card).init('Ten', suitType.Heart, 10, 'images/classic-cards/10_of_hearts_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
-                deckOfCard.push(Object.create(Card).init('Ten', suitType.Spade, 10, 'images/classic-cards/10_of_spades_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
-                deckOfCard.push(Object.create(Card).init('Nine', suitType.Diamond, 9, 'images/classic-cards/09_of_diamonds.svg', 'sounds/cardPlace3.wav'));
-                deckOfCard.push(Object.create(Card).init('Nine', suitType.Clubs, 9, 'images/classic-cards/09_of_clubs.svg', 'sounds/cardPlace3.wav'));
-                deckOfCard.push(Object.create(Card).init('Nine', suitType.Heart, 9, 'images/classic-cards/09_of_hearts.svg', 'sounds/cardPlace3.wav'));
-                deckOfCard.push(Object.create(Card).init('Nine', suitType.Spade, 9, 'images/classic-cards/09_of_spades.svg', 'sounds/cardPlace3.wav'));
-                deckOfCard.push(Object.create(Card).init('Eight', suitType.Diamond, 8, 'images/classic-cards/08_of_diamonds.svg', 'sounds/cardPlace4.wav'));
-                deckOfCard.push(Object.create(Card).init('Eight', suitType.Clubs, 8, 'images/classic-cards/08_of_clubs.svg', 'sounds/cardPlace4.wav'));
-                deckOfCard.push(Object.create(Card).init('Eight', suitType.Heart, 8, 'images/classic-cards/08_of_hearts.svg', 'sounds/cardPlace4.wav'));
+                deckOfCard.push(Object.create(Card).init('Ace', suitType.Diamond, 14, 'Images/classic-cards/01_of_diamonds_A.svg', 'sounds/cardPlace1.wav'));
+                deckOfCard.push(Object.create(Card).init('Ace', suitType.Clubs, 14, 'Images/classic-cards/01_of_clubs_A.svg', 'sounds/cardPlace2.wav'));
+                deckOfCard.push(Object.create(Card).init('Ace', suitType.Heart, 14, 'Images/classic-cards/01_of_hearts_A.svg', 'sounds/cardPlace3.wav'));
+                deckOfCard.push(Object.create(Card).init('Ace', suitType.Spade, 14, 'Images/classic-cards/01_of_spades_A.svg', 'sounds/cardPlace4.wav'));
+                deckOfCard.push(Object.create(Card).init('King', suitType.Diamond, 13, 'Images/classic-cards/King_of_diamonds2.svg', 'sounds/cardPlace1.wav'));
+                deckOfCard.push(Object.create(Card).init('King', suitType.Clubs, 13, 'Images/classic-cards/King_of_clubs2.svg', 'sounds/cardPlace2.wav'));
+                deckOfCard.push(Object.create(Card).init('King', suitType.Heart, 13, 'Images/classic-cards/King_of_hearts2.svg', 'sounds/cardPlace3.wav'));
+                deckOfCard.push(Object.create(Card).init('King', suitType.Spade, 13, 'Images/classic-cards/King_of_spades2.svg', 'sounds/cardPlace4.wav'));
+                deckOfCard.push(Object.create(Card).init('Queen', suitType.Diamond, 12, 'Images/classic-cards/Queen_of_diamonds2.svg', 'sounds/cardPlace1.wav'));
+                deckOfCard.push(Object.create(Card).init('Queen', suitType.Clubs, 12, 'Images/classic-cards/Queen_of_clubs2.svg', 'sounds/cardPlace2.wav'));
+                deckOfCard.push(Object.create(Card).init('Queen', suitType.Heart, 12, 'Images/classic-cards/Queen_of_hearts2.svg', 'sounds/cardPlace3.wav'));
+                deckOfCard.push(Object.create(Card).init('Queen', suitType.Spade, 12, 'Images/classic-cards/Queen_of_spades2.svg', 'sounds/cardPlace4.wav'));
+                deckOfCard.push(Object.create(Card).init('Jack', suitType.Diamond, 11, 'Images/classic-cards/Jack_of_diamonds2.svg', 'sounds/cardPlace1.wav'));
+                deckOfCard.push(Object.create(Card).init('Jack', suitType.Clubs, 11, 'Images/classic-cards/Jack_of_clubs2.svg', 'sounds/cardPlace1.wav'));
+                deckOfCard.push(Object.create(Card).init('Jack', suitType.Heart, 11, 'Images/classic-cards/Jack_of_hearts2.svg', 'sounds/cardPlace1.wav'));
+                deckOfCard.push(Object.create(Card).init('Jack', suitType.Spade, 11, 'Images/classic-cards/Jack_of_spades2.svg', 'sounds/cardPlace1.wav'));
+                deckOfCard.push(Object.create(Card).init('Ten', suitType.Diamond, 10, 'Images/classic-cards/10_of_diamonds_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
+                deckOfCard.push(Object.create(Card).init('Ten', suitType.Clubs, 10, 'Images/classic-cards/10_of_clubs_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
+                deckOfCard.push(Object.create(Card).init('Ten', suitType.Heart, 10, 'Images/classic-cards/10_of_hearts_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
+                deckOfCard.push(Object.create(Card).init('Ten', suitType.Spade, 10, 'Images/classic-cards/10_of_spades_-_David_Bellot.svg', 'sounds/cardPlace2.wav'));
+                deckOfCard.push(Object.create(Card).init('Nine', suitType.Diamond, 9, 'Images/classic-cards/09_of_diamonds.svg', 'sounds/cardPlace3.wav'));
+                deckOfCard.push(Object.create(Card).init('Nine', suitType.Clubs, 9, 'Images/classic-cards/09_of_clubs.svg', 'sounds/cardPlace3.wav'));
+                deckOfCard.push(Object.create(Card).init('Nine', suitType.Heart, 9, 'Images/classic-cards/09_of_hearts.svg', 'sounds/cardPlace3.wav'));
+                deckOfCard.push(Object.create(Card).init('Nine', suitType.Spade, 9, 'Images/classic-cards/09_of_spades.svg', 'sounds/cardPlace3.wav'));
+                deckOfCard.push(Object.create(Card).init('Eight', suitType.Diamond, 8, 'Images/classic-cards/08_of_diamonds.svg', 'sounds/cardPlace4.wav'));
+                deckOfCard.push(Object.create(Card).init('Eight', suitType.Clubs, 8, 'Images/classic-cards/08_of_clubs.svg', 'sounds/cardPlace4.wav'));
+                deckOfCard.push(Object.create(Card).init('Eight', suitType.Heart, 8, 'Images/classic-cards/08_of_hearts.svg', 'sounds/cardPlace4.wav'));
 
                 return deckOfCard.slice();
 
@@ -460,51 +460,53 @@ var Game = (function () {
         alert.render(msg);
     }
 
-    var dialogBox = document.createElement('div'),
-        dialogOverLay = document.createElement('div'),
-        nestedDialogBox = document.createElement('div'),
-        dialogAlertHeader = document.createElement('div'),
-        dialogAlertBody = document.createElement('div'),
-        dialogAlertFooter = document.createElement('div');
 
-    dialogBox.appendChild(nestedDialogBox);
-    nestedDialogBox.appendChild(dialogAlertHeader);
-    nestedDialogBox.appendChild(dialogAlertBody);
-    nestedDialogBox.appendChild(dialogAlertFooter);
-    dialogOverLay.style.display = 'none';
-    dialogOverLay.style.opacity = '0.8';
-    dialogOverLay.style.position = 'fixed';
-    dialogOverLay.style.top = '0px';
-    dialogOverLay.style.left = '0px';
-    dialogOverLay.style.background = '#FFF';
-    dialogOverLay.style.width = '100%';
-    dialogOverLay.style.zIndex = '10';
-    dialogBox.style.display = 'none';
-    dialogBox.style.position = 'fixed';
-    dialogBox.style.background = '#000';
-    dialogBox.style.borderRadius = '9px';
-    dialogBox.style.width = '550px';
-    dialogBox.style.zIndex = '10';
-    dialogAlertHeader.style.background = '#666';
-    dialogAlertHeader.style.fontSize = '19px';
-    dialogAlertHeader.style.padding = '10px';
-    dialogAlertHeader.style.color = '#CCC';
-    dialogAlertHeader.style.borderTopLeftRadius = '10px';
-    dialogAlertHeader.style.borderTopRightRadius = '10px';
-    dialogAlertBody.style.background = '#333';
-    dialogAlertBody.style.padding = '20px';
-    dialogAlertBody.style.color = '#FFF';
-    dialogAlertFooter.style.background = '#666';
-    dialogAlertFooter.style.padding = '10px';
-    dialogAlertFooter.style.textAlign = 'right';
-    dialogAlertFooter.style.borderBottomLeftRadius = '10px';
-    dialogAlertFooter.style.borderBottomRightRadius = '10px';
-    document.body.appendChild(dialogOverLay);
-    document.body.appendChild(dialogBox);
+
 
     var alert = new CustomAlert();
 
     function CustomAlert() {
+        var dialogBox = document.createElement('div'),
+            dialogOverLay = document.createElement('div'),
+            nestedDialogBox = document.createElement('div'),
+            dialogAlertHeader = document.createElement('div'),
+            dialogAlertBody = document.createElement('div'),
+            dialogAlertFooter = document.createElement('div');
+
+        dialogBox.appendChild(nestedDialogBox);
+        nestedDialogBox.appendChild(dialogAlertHeader);
+        nestedDialogBox.appendChild(dialogAlertBody);
+        nestedDialogBox.appendChild(dialogAlertFooter);
+        dialogOverLay.style.display = 'none';
+        dialogOverLay.style.opacity = '0.8';
+        dialogOverLay.style.position = 'fixed';
+        dialogOverLay.style.top = '0px';
+        dialogOverLay.style.left = '0px';
+        dialogOverLay.style.background = '#FFF';
+        dialogOverLay.style.width = '100%';
+        dialogOverLay.style.zIndex = '10';
+        dialogBox.style.display = 'none';
+        dialogBox.style.position = 'fixed';
+        dialogBox.style.background = '#000';
+        dialogBox.style.borderRadius = '9px';
+        dialogBox.style.width = '550px';
+        dialogBox.style.zIndex = '10';
+        dialogAlertHeader.style.background = '#666';
+        dialogAlertHeader.style.fontSize = '19px';
+        dialogAlertHeader.style.padding = '10px';
+        dialogAlertHeader.style.color = '#CCC';
+        dialogAlertHeader.style.borderTopLeftRadius = '10px';
+        dialogAlertHeader.style.borderTopRightRadius = '10px';
+        dialogAlertBody.style.background = '#333';
+        dialogAlertBody.style.padding = '20px';
+        dialogAlertBody.style.color = '#FFF';
+        dialogAlertFooter.style.background = '#666';
+        dialogAlertFooter.style.padding = '10px';
+        dialogAlertFooter.style.textAlign = 'right';
+        dialogAlertFooter.style.borderBottomLeftRadius = '10px';
+        dialogAlertFooter.style.borderBottomRightRadius = '10px';
+        document.body.appendChild(dialogOverLay);
+        document.body.appendChild(dialogBox);
         this.render = function (dialog) {
             var winW = window.innerWidth;
             var winH = window.innerHeight;
@@ -540,10 +542,10 @@ var Game = (function () {
         $("#btnChoosePot3").remove();
 
         var pot = currentThreePots.firstPot;
-        for (var i = 0; i < NUMBER_OF_CARDS.POT; i+=1) {
+        for (var i = 0; i < NUMBER_OF_CARDS.POT; i += 1) {
             pot.push(currentThreePots.secondPot[i]);
         }
-        for (var i = 0; i < NUMBER_OF_CARDS.POT; i+=1) {
+        for (var i = 0; i < NUMBER_OF_CARDS.POT; i += 1) {
             pot.push(currentThreePots.thirdPot[i]);
         }
         var potToDraw = pot.slice(0);
